c181688aefbd0178f0e8b43f3e4055cee128aca5
remynaps <remysije@gmail.com>
2019-06-10 19:39:25 +0200

    random string and better url encoding

--- server/api.py
+++ server/api.py
@@ -2,6 +2,8 @@ import datetime
 from flask import Flask, render_template, redirect
 from flask import Blueprint, request, jsonify, abort
 from config import config
+from urllib.parse import quote, urlencode
+from random_string import generate_random
 
 app = Flask(__name__)
 app.config.update(**config)
@@ -15,7 +17,9 @@ def login():
     app_id = app.config['APP_ID']
     app_secret = app.config['APP_SECRET']
     redirect_url = app.config['REDIRECT_URL']
-    return redirect(app.config['AUTH_URL'] + '?client_id=' + app_id + '&client_secret=' + app_secret + '&redirect_url=' + redirect_url + '&state=ransomshits', code=302)
+    state = generate_random(10)
+    params = {'client_id': app_id, 'client_secret': app_secret, 'redirect_url': redirect_url,  'state': state}
+    return redirect(app.config['AUTH_URL'] + '?' + urlencode(params, quote_via=quote), code=302)
 
 @app.route("/callback")
 def loggedin():
