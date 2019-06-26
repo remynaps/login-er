3257ab4f7006a5ffb5d569ae03e24adbb13e2efa
remynaps <remysije@gmail.com>
2019-06-27 00:05:24 +0200

    added error page

--- server/api.py
+++ server/api.py
@@ -32,3 +32,8 @@ def loggedin():
 @app.route("/logout")
 def logout():
     return redirect(app.config['AUTH_URL'] + '/logout')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
